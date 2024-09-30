54ba275468ee50595c6f541ddec3671e07a30f22
adev07 <adityaanand1245@gmail.com>
2024-09-30 23:49:39 +0530

    table entries

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { Table as Tab } from 'antd'
+import React from 'react';
+import { Table as AntTable } from 'antd';
 
 type Props = {}
 
 const Table = (props: any) => {
+  // Function to apply zebra striping
+  const rowClassName = (record: any, index: number) => {
+    return index % 2 === 0 ? 'table-row-even' : 'table-row-odd';
+  };
+
   return (
-    <Tab {...props} pagination={false} style={{border:"1px solid #e0e2e7", borderRadius:"8px"}} />
-  )
+    <AntTable
+      {...props}
+      pagination={false}
+      rowClassName={rowClassName} // Apply row class name for zebra pattern
+      style={{ border: "1px solid #e0e2e7", borderRadius: "8px" }}
+    />
+  );
 }
 
-export default Table
+export default Table;
